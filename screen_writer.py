c3f93e804acace732c050593a6cf536f199b970d
saltchicken <john.eicher89@gmail.com>
2023-05-23 21:27:42 -0700

    Center the window

--- screen_writer.py
+++ screen_writer.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QLabel
 from PyQt5.QtCore import Qt, QTimer
 from multiprocessing import Process
 
@@ -8,7 +8,11 @@ class OverlayWindow(QWidget):
         super().__init__()
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool)
         self.setAttribute(Qt.WA_TranslucentBackground)
+        self.width = 200
+        self.height = 100
+        screen = QDesktopWidget().screenGeometry()
         self.setGeometry(100, 100, 200, 100)
+        self.move((screen.width()-self.width)/2, (screen.height()-self.height)/2)
         
         self.label = QLabel(text, self)
         self.label.setStyleSheet("font-size: 20px; color: white;")
