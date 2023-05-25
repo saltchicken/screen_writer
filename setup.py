c2513e66ad7d430add54f1cc9a897d30822cce35
saltchicken <john.eicher89@gmail.com>
2023-05-24 20:06:15 -0700

    Add clear and append to OverlayController.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='screen_writer',
-    version='0.0.8',
+    version='0.0.9',
     author='John Eicher',
     author_email='john.eicher89@gmail.com',
     description='Testing installation of Package',
