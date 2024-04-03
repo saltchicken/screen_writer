092ceb00de3f0941d8f07e869cb7309f1178d414
saltchicken <john.eicher89@gmail.com>
2024-04-02 21:06:36 -0700

    Add systray

--- screen_writer_socket_server.py
+++ screen_writer_socket_server.py
@@ -1,30 +1,30 @@
 import sys, time
 import configargparse
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QLabel, QStyle, QAction, QMenu, QSystemTrayIcon
 from PyQt5.QtCore import Qt, QTimerEvent
 
 import socket, threading
 
 class ServerThread(threading.Thread):
-    def __init__(self, label):
+    def __init__(self, label, quit_event):
         super().__init__()
         self.label = label
+        self.quit_event = quit_event
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind(('0.0.0.0', 12346))
         self.server_socket.listen(1)
 
     def run(self):
-        while True:
-            client_socket, client_address = self.server_socket.accept()
-            print('Connection from', client_address)
-            while True:
-                try:
-                    message = client_socket.recv(4096).decode()
-                    self.label.setText(message)
-                except Exception as e:
-                    print(e)
-                # finally:
-                #     client_socket.close()
+        client_socket, client_address = self.server_socket.accept()
+        client_socket.settimeout(1) # Set timeout so that while loop does not block
+        print('Connection from', client_address)
+        while not self.quit_event.is_set():
+            try:
+                message = client_socket.recv(4096).decode()
+                self.label.setText(message)
+            except socket.timeout:
+                pass
+        client_socket.close()
 
 class OverlayWindow(QWidget):
     def __init__(self, args):
@@ -42,17 +42,48 @@ class OverlayWindow(QWidget):
         self.label = QLabel(self)
         self.label.setStyleSheet("font-size: 20px; color: white;")
         self.label.setGeometry(0, 0, self.width, self.height) 
-        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setAlignment(Qt.AlignLeft)
+        
+        self.tray_menu = QMenu()
+                
+        self.clear_action_checkbox = QAction('Clear', self)
+        self.clear_action_checkbox.setCheckable(True)
+        self.clear_action_checkbox.triggered.connect(self.clear_action)
+        self.tray_menu.addAction(self.clear_action_checkbox)
+        
+        self.quit_action_checkbox = QAction('Quit', self)
+        self.quit_action_checkbox.setCheckable(True)
+        self.quit_action_checkbox.triggered.connect(self.quit_action)
+        self.tray_menu.addAction(self.quit_action_checkbox)
+        
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(self.style().standardIcon(QStyle.SP_ComputerIcon))
+        self.tray_icon.setToolTip("Tasker")
+        self.tray_icon.setContextMenu(self.tray_menu)
+        self.tray_icon.show()
         
         self.start_server()
         
-
     def center_window(self):
         self.move(int((self.screen.width()-self.width)/2), int((self.screen.height()-self.height)/2))
         
     def start_server(self):
-        self.server_thread = ServerThread(self.label)
+        self.quit_event = threading.Event()
+        self.server_thread = ServerThread(self.label, self.quit_event)
         self.server_thread.start()
+                
+    def quit_action(self):
+        checked = self.quit_action_checkbox.isChecked()
+        if checked:
+            self.quit_event.set()
+            QApplication.quit()
+        else:
+            print('This should have never been reached')
+            
+    def clear_action(self):
+        checked = self.clear_action_checkbox.isChecked()
+        if checked:
+            self.label.setText('')
           
         
 
